0ee80855639f9640e08bc49617c5ae0908e3cfe2
awesome13524 <94251418+awesome13524@users.noreply.github.com>
2023-08-12 13:38:33 -0400

    Create function templates
    Created three functions with comments describing their use

---
+++ rps.js
@@ -0,0 +1,17 @@
+// Function that chooses computer input
+function getComputerChoice(){
+    // Randomly returns rock, paper, or scissors
+    return
+}
+
+// Function that plays a single round
+// Takes player input and computer input as parameters
+function playRound(){
+    // Returns message depending on winner
+    return
+}
+// Function that plays a 5 round game
+function game(){
+    // Uses single round function to play 5 rounds, keeps score, and returns a winner at the end
+    return
+}
