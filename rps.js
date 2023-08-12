cc559b86907567c85b11ba6243b043524dd7c5f9
awesome13524 <94251418+awesome13524@users.noreply.github.com>
2023-08-12 14:13:29 -0400

    Add pseudocode for game function
    Pseudocoded player input, round playing, and winning messages in the
    game function

--- rps.js
+++ rps.js
@@ -1,17 +1,38 @@
-// Function that chooses computer input
+// Generate computer input
 function getComputerChoice(){
     // Randomly returns rock, paper, or scissors
-    return
+
 }
 
-// Function that plays a single round
-// Takes player input and computer input as parameters
-function playRound(){
-    // Returns message depending on winner
-    return
+// Play a single round
+// Take player input and computer input as parameters
+function playRound(playerSelection, computerSelection){
+    // Return message depending on winner
 }
-// Function that plays a 5 round game
+// Play a 5 round game
 function game(){
-    // Uses single round function to play 5 rounds, keeps score, and returns a winner at the end
-    return
+    // Initialize player and computer scores
+    
+    // Play rounds until the sum of the scores equals 5
+    while(){
+        // Prompt player for input until they input a proper value
+        while(){
+
+        }
+        // Play a round
+        switch(playRound()){
+            // If the computer wins add 1 to its score and tell the player they lost the round
+            
+            // If the player wins add 1 to their score and tell the player they won the round
+
+            //If it's a tie tell the player it's a tie
+        }
+
+    }
+    // Display message depending on game winner
+    switch(){
+
+    }
 }
+
+game();
