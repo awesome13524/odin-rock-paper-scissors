6d450817879d131e05cb44838d986e6137bb0ce5
awesome13524 <94251418+awesome13524@users.noreply.github.com>
2023-08-13 11:23:59 -0400

    Complete game function
    Added round results and a final victory/defeat message.

--- js/rps.js
+++ js/rps.js
@@ -1,6 +1,6 @@
 // Generate computer input
 function getComputerChoice(){
-    // Randomly generate a number from 0 to 2
+    // Randomly generate number from 0 to 2
     const num = Math.floor(Math.random() * 3);
     // Return choice based on number
     switch(num){
@@ -16,32 +16,32 @@ function getComputerChoice(){
 // Play a single round
 // Take player input and computer input as parameters
 function playRound(playerSelection, computerSelection){
-    // Return message depending on winner
+    // Return result of game
     if(playerSelection === computerSelection){
         return "tie"
     }
     if(playerSelection === "rock"){
         switch(computerSelection){
             case "paper":
-                return "lose"
+                return "lose";
             case "scissors":
-                return "win"
+                return "win";
         }
     }
     if(playerSelection === "paper"){
         switch(computerSelection){
             case "rock":
-                return "win"
+                return "win";
             case "scissors":
-                return "lose"
+                return "lose";
         }
     }
     if(playerSelection === "scissors"){
         switch(computerSelection){
             case "rock":
-                return "lose"
+                return "lose";
             case "paper":
-                return "win"
+                return "win";
         }
     }
 }
@@ -54,26 +54,50 @@ function game(){
     while(playerScore < 5 && computerScore < 5){
         // Prompt player for input until they input a proper value
         let keepGoing = true;
-        let playerChoice
+        let playerChoice;
         while(keepGoing){
             playerChoice = prompt("Rock, paper, or scissors?" , "").toLowerCase;
             if(playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors"){
                 keepGoing = false;
             }
         }
+        // Generate computer input
+        let computerChoice = getComputerChoice();
         // Play a round
-        
-            //If it's a tie tell the player it's a tie
-
-            // If the player wins add 1 to their score and tell the player they won the round
-
-            // If the computer wins add 1 to its score and tell the player they lost the round
-            
-        
-
+        let result = playRound(playerChoice, computerChoice)
+        switch(result){
+            //If tie tell player they tied the round
+            case "tie":
+                console.log("You chose: " + playerChoice);
+                console.log("The computer chose: " + computerChoice);
+                console.log("It's a tie");
+                console.log("Current score is player: " + playerScore + " computer: " + computerScore);
+                break;
+            // If player wins add 1 to their score and tell player they won the round
+            case "win":
+                console.log("You chose: " + playerChoice);
+                console.log("The computer chose: " + computerChoice);
+                console.log("You won the round");
+                ++playerScore;
+                console.log("Current score is player: " + playerScore + " computer: " + computerScore);
+                break;
+            // If computer wins add 1 to its score and tell player they lost the round
+            case "lose":
+                console.log("You chose: " + playerChoice);
+                console.log("The computer chose: " + computerChoice);
+                console.log("You lost the round");
+                ++computerScore;
+                console.log("Current score is player: " + playerScore + " computer: " + computerScore);
+                break;
+        }
     }
     // Display message depending on game winner
-    
+    if(playerScore === 5){
+        console.log("Congratulations! You won the game!")
+    }
+    if(computerScore === 5){
+        console.log("Game Over!")
+    }
 }
 
 console.log(game());
